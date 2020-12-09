a576970cd1866bed43cf48758b01b9ea0eb1f120
hikaru4215 <hikaru4215hkr2178ay@yahoo.co.jp>
2020-12-10 00:51:12 +0900

    view

--- app/views.py
+++ app/views.py
@@ -7,6 +7,11 @@ from django.http import HttpResponse, JsonResponse
 from django.core.mail import BadHeaderError, EmailMessage, send_mail
 from django.contrib.auth.mixins import LoginRequiredMixin
 from accounts.models import CustomUser
+from django.db.models import Q, Avg
+from functools import reduce
+from operator import and_
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
+import textwrap
 
 class IndexView(View):
 	def get(self, request, *args, **kwargs):
