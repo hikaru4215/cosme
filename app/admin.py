65a41eebc1acc4e6b61abf1e6c42cf51238b6286
hikaru4215 <hikaru4215hkr2178ay@yahoo.co.jp>
2020-12-10 11:26:17 +0900

    img

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Recommenditem, Category, Price, Score
+from .models import Recommenditem, Category, Price, Score, Review
 
 admin.site.register(Recommenditem)
+admin.site.register(Review)
 admin.site.register(Category)
 admin.site.register(Price)
 admin.site.register(Score)
