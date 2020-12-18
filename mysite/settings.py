8fd6a7f4d65cdef3dbea3bd7f39e9e3b93c5e9af
hikaru <hikaru@mdp.local>
2020-12-18 23:38:28 +0900

    修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
