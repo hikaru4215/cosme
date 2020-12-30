353a0b5c6c1e51c969f3341f3a5b38bb67cac996
hikaru <hikaru@mdp.local>
2020-12-30 18:24:47 +0900

    settigs

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,13 +18,11 @@ from django.urls import path, include
 
 from django.conf.urls.static import static 
 from django.conf import settings 
-from django.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('accounts/', include('accounts.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     # path('accounts/', include('allauth.urls')),
 ]
 
